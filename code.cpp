02236f7154e9c1dad89e8b82e66fa3dee5a6adb7
Amaro <aalarconl@usm.cl>
2024-05-27 23:15:01 -0400

    intento de funciones

--- code.cpp
+++ code.cpp
@@ -3,35 +3,69 @@
 #include <string>
 using namespace std;
 
-class super_string {
+ class super_string {
  private:
-  struct nodo {
-    nodo *left = nullptr, *right = nullptr;
-    int index;
-    char c;
-    nodo(int index, char c) {}
-  };
-  int height = 0; // Altura del árbol
-  int length = 0; // Largo del super-string
-  nodo* root = nullptr; // Raíz del super-string
+ struct nodo {
+      nodo *left = nullptr, *right = nullptr;
+ int index;
+ char c;
+ nodo(int index, char c) {}
+ nodo(){}
+ };
+ int height = 0; // Altura del árbol
+ int length = 0; // Largo del super-string
+    nodo* root = nullptr; // Raíz del super-string
  public:
-  super_string() {}
-  void juntar(super_string &s);
-  void agregar(char c); // Insertar un caracter en la última posición
-  // En la izquierda esta el super_string a y en la derecha el super_string b
-  void separar(int i, super_string &a, super_string &b);
-  void reverso(); // No debe cambiar la altura del árbol
-  int recortar(); // Retorna this->height después de recortar
-  string stringizar(); // Debe ser O(n)
-  void limpiar(); // Se deben borrar todos los nodos del super-string
+ super_string() {}
+ void juntar(super_string &s);
+ void agregar(char c) {
+ }; // Insertar un caracter en la última posición
+ // En la izquierda esta el super_string a y en la derecha el super_string b
+ void separar(int i, super_string &a, super_string &b);
+ void reverso(); // No debe cambiar la altura del árbol
+ int recortar(); // Retorna this->height después de recortar
+    string stringizar(); // Debe ser O(n)
+ void limpiar(); // Se deben borrar todos los nodos del super-string
  };
 
+int INSERTAR(string x, string y, super_string &s) {
+    for (int i = 10; i < x.size(); i++) {
+        s.agregar(x[i]);
+    }
+    return 0;
+}
 
-int main() { // Función main, ingreso Y salida de datos
-    string a = "x";
-    while ( a != "FIN"){
-        getline(cin, a);
+void mostrar(super_string &s) {
+    cout << s.stringizar() << endl;
+}
 
-    };
+int analisis(string a) {
+    string funcion = "";
+    super_string s1;
+    for (int i = 0; i < a.size(); i++) {
+        char k = a[i];
+        if (k == ' ') {
+            break;
+        }
+        else {
+            funcion += k;
+        }
+    }
+    if (funcion == "INSERTAR") {
+        INSERTAR(a, funcion, s1);
+    }
+    // Implementa las otras operaciones aquí
+    if (funcion == "MOSTRAR") {
+        mostrar(s1);
+    }
+    return 0;
+}
+
+int main() {
+    string input = "x";
+    while (input != "FIN") {
+        getline(cin, input);
+        analisis(input);
+    }
     return 0;
 }
