29238f582416e23ba9dd8a272fd54b9b198d817f
Mxim0 <166274216+Mxim0@users.noreply.github.com>
2024-06-02 06:23:29 -0400

    TDAtarea2.cpp

---
+++ TDAtarea2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class super_string {
+    private:
+        struct nodo {
+            nodo *left = nullptr, *right = nullptr;
+            int index;
+            char c;
+            nodo(int index, char c) : index(index), c(c) {} // REVISAR, YA QUE TUVE QUE PEDIR AYUDA A COPILOT, Y DICE QUE SIN ESTO NO SE INICIALIZA LOS VALORES
+            nodo(){}
+        };
+        int height = 0; // Altura del árbol
+        int length = 0; // Largo del super-string
+        nodo* root = nullptr; // Raíz del super-string
+        void InOrdenRec(nodo* raiz, string &text);
+        void limpiarRec(nodo* raiz);
+    public:
+        super_string() {}
+        void juntar(string frase1, string frase2, super_string &s);
+        void agregar(char c); // Insertar un caracter en la última posición
+        void separar(int i, super_string &a, super_string &b); // En la izquierda esta el super_string a y en la derecha el super_string b
+        void reverso(int inicio , int fin, super_string &b); // No debe cambiar la altura del árbol
+        int recortar(); // Retorna this->height después de recortar
+        string stringizar(); // Debe ser O(n)
+        void limpiar(); // Se deben borrar todos los nodos del super-string
+};
+
