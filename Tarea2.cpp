8bdb2bf2e8b82954be5e91269f526777c08bc129
Mxim0 <166274216+Mxim0@users.noreply.github.com>
2024-06-02 06:43:59 -0400

    Tarea2.cpp
    Agregué las funciones stringizar, limpiar, juntar y reverso.
    - stringizar funciona 10/10
    - limpiar 10/10 (pero no estoy 100% seguro de que se haga así)
    - juntar esta en un limbo
    - reverso esta a medio hacer
    DUDA la funcion agregar no deberia considerar el caso en que se le agrege algun caracter por el medio???

--- Tarea2.cpp
+++ Tarea2.cpp
@@ -5,14 +5,14 @@
 #include <sstream>
 using namespace std;
 
-void super_string::InOrdenRec( nodo* nodo, string& text){
+void super_string::InOrdenRec( nodo* nodo, string& text){  // Recorre el arbol mientras crea un string con los caracteres de cada nodo
     if (nodo == nullptr) return;
     InOrdenRec ( nodo -> left, text ); 
     text +=  nodo->c ;
     InOrdenRec ( nodo -> right, text );
 }
 
-void super_string::limpiarRec( nodo* raiz){
+void super_string::limpiarRec( nodo* raiz){     // Elimina el arbol empezando por la hoja izquierda 
     limpiarRec(raiz->left);
     limpiarRec(raiz->right);
     delete raiz;
@@ -29,14 +29,14 @@ void super_string::agregar(char c) {  // Insertar un caracter en la última posi
             current_nodo = current_nodo->right;
         }
         current_nodo -> right = nuevo_nodo;
-        //nuevo_nodo ->left = current_nodo; // Nose que hace, pero cuando llamaba el InOrdenMod se quedaba en bucle
+        //nuevo_nodo ->left = current_nodo;  // NOSE QUE HACE, PERO CUANDO LLAMABA A INORDENREC SE QUEDABA EN BUCLE
     }
     length++;
 }
 
 void super_string::juntar(string frase1, string frase2, super_string &s) { // NO ESTOY SEGURO DE QUE TENGA QUE SER DE ESTA FORMA PERO XDD
-    string a = frase1 + frase2;
-    for ( int i = 0; i < a.size(); i++){
+    string a = frase1 + frase2;                                            
+    for ( int i = 0; i < a.size(); i++){     // CREO QUE TALVEZ DEBA RECIBIR UN SOLO STRING, ESTE SE AGREGA AL ARBOL ACTUAL Y SE DEVUELVE UN NUEVO SUPERSTRING
             char caracter = a[i];
             agregar( caracter );
     }
@@ -59,13 +59,13 @@ string super_string::stringizar(){  // Debe ser O(n)
 
 void super_string::limpiar(){  // Se deben borrar todos los nodos del super-string
     limpiarRec(root);
-    root = nullptr;
+    root = nullptr;            // LA HICE DE IMPROVISO, PARECE FUNCIONAR BIEN
     length = 0;
     height = 0;
 }
 
 void super_string::reverso(int inicio , int fin, super_string &b){  // No debe cambiar la altura del árbol
-    string texto = stringizar();
+    string texto = stringizar();                                   // FALTA EL CASO EN QUE INICIO Y FIN SEAN UN SUBSTRING DEL SUPERSTRNG
     super_string nuevo;
     if (inicio == 0 && fin == texto.size() - 1){
         for (fin; fin != -1; fin--){
@@ -101,18 +101,16 @@ void analisis(string a, super_string &b){
                 break;
             }
         }
-        istringstream iss(aux);
+        istringstream iss(aux);          // CAMBIE LA FORMA EN QUE LEE LOS NUMERO PARA QUE PUEDA LEER MASS DE UNO TIPO "17" Y NO LEA SOLO "1"
         int donde_insertar;
         if (iss >> donde_insertar) {
             for (f; f < a.size(); f++){
                 char caracter = a[f];
                 b.agregar(caracter);
             }
-        } else {
-            cerr << "Error al convertir la cadena en enteros." << endl;
-        }
+        } 
     }
-    if (funcion == "JUNTAR"){                           // JUNTAR
+    if (funcion == "JUNTAR"){                           // JUNTAR !!PUEDE QUE ESTA NO SEA LA FUNCION QUE PIDEN¡¡
         int donde_insertar = a[7];
         super_string nuevo;
         string super1 = "", super2 = "";
@@ -142,9 +140,7 @@ void analisis(string a, super_string &b){
         istringstream iss(numeros);
         if (iss >> inicio >> fin) {
             b.reverso(inicio, fin, b);
-        } else {
-            cerr << "Error al convertir la cadena en enteros." << endl;
-        }
+        } 
     }
 }
 
@@ -152,7 +148,7 @@ int main() {
     fstream archivo;
     string input;
     super_string arbol;
-    archivo.open( "Algo.txt", ios::in );
+    archivo.open( "Algo.txt", ios::in );      // PARA QUE LEA ARCHIVOS 
     if ( archivo.fail() ){
         cout << "No se pudo abrir el archivo";
     }
