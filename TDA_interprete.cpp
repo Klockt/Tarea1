c78ea07dca861afca50a119d34553af5884fad90
Mxim0 <166274216+Mxim0@users.noreply.github.com>
2024-08-24 04:08:12 -0400

    Agregué los cambios al constructor

--- TDA_interprete.cpp
+++ TDA_interprete.cpp
@@ -6,7 +6,7 @@ class Interprete {
         Programa * programas ;
         int * salida ;
     public :
-        Interprete (int cant_programas );
+        Interprete (int cant_programas , int largo_salidas);
         void cargar_programa (int n);
         void ejecutar_programa () ;
         void mostrar_programa_cargado () ;
