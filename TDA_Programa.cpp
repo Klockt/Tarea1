1348067613d4d27d0f780668bcd79d966f8135a1
Mxim0 <166274216+Mxim0@users.noreply.github.com>
2024-08-24 04:10:17 -0400

    Hice los cambios

--- TDA_Programa.cpp
+++ TDA_Programa.cpp
@@ -1,15 +1,15 @@
 class Programa {
     private :
         int largo_operaciones ;
-        int * puntero_operaciones ;
+        char * puntero_operaciones ;
         int * puntero_salida ;
         char * operaciones ;
     public :
+        Programa ( int largo_operaciones );
         void ejecutar_operador () ;
         void mover ( char dir );
         void asignar ( int valor , int * salida );
         char obtener () ;
         void terminar_programa () ;
-        Programa ( int largo_operaciones , int largo_resultado );
         void ejecutar () ;
 };
