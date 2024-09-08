1e2c5d35d32f82d3701cfb6339b2171035cebfc1
Amaro <aalarconl@usm.cl>
2024-09-07 21:07:07 -0400

    agrege el arreglo

--- Tarea3.cpp
+++ Tarea3.cpp
@@ -19,7 +19,13 @@ Programa::~Programa() {
 }
 
 void Programa::ejecutar_operador() {
-        // Implementar la lógica para ejecutar un operador específico
+        char list = new char[' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+                             'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                             'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                             'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'V', 'W',
+                             'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8',
+                             '9', '.', ':', '+', '-', '<', '>', '[', ']', '!'];
+        
 }
 
 void Programa::mover(char dir) {
@@ -97,6 +103,7 @@ void Interprete::cargar_programa(int n) {
             std::cout << "Programa " << n << " cargado.\n";
         } else {
             std::cout << "Número de programa inválido.\n";
+            programas[cargado].mostrar();
         }
     }
 
