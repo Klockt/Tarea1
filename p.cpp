b12d4eda04c24f68ef459a63b37da6ddece20d11
Amaro <aalarconl@usm.cl>
2024-10-18 16:10:43 -0300

    base main

--- p.cpp
+++ p.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include "Director.cpp"
+#include "Arbol.cpp"
+using namespace std;
+
 
 int main() {
-    
+    ifstream archivo("Peliculas.txt");
+    if (!archivo){
+        cout << "No se logró encontrar el archivo" << endl;
+        return 1;
+    }
+    string linea;
+    while ( getline(archivo,linea)){
+        cout << linea << endl;
+    }
+    return 0;
 }
