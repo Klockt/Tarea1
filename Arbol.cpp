def7229f5bfd00a8909bb35597b93831ad9334bf
Amaro <aalarconl@usm.cl>
2024-10-25 18:46:46 -0300

    si

--- Arbol.cpp
+++ Arbol.cpp
@@ -8,7 +8,16 @@ Arboles::Arboles():root_1(nullptr), root_2(nullptr), size_1(0), size_2(0) {}
 
 Arboles::~Arboles(){}
 
-void Arboles::insertar_pelicula( Pelicula * pelicula){}
+void Arboles::insertar_pelicula( Pelicula * pelicula ){
+    Director* dir = buscar_director(pelicula->director);
+        if (!dir) {
+            dir = new Director();
+            dir->set_nombre_director(pelicula->director);
+            insertar_en_arbol(root_1, dir);
+        }
+        dir->agregar_pelicula(pelicula);
+        dir->ordenar();
+}
 
 void Arboles::copiar_arbol(){}
 
