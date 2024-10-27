f4bc8b91148a5e0908295a738d6f768c787676cf
Mxim0 <166274216+Mxim0@users.noreply.github.com>
2024-10-27 18:36:40 -0300

    xd

--- p.cpp
+++ p.cpp
@@ -4,14 +4,6 @@
 #include "TDAS.cpp"
 using namespace std;
 
-
-struct Pelicula {
-    string nombre ;
-    string director ;
-    float rating ;
-};
-
-
 int main() {
     Arboles arbol;
     ifstream archivo("Peliculas.txt");
@@ -23,7 +15,7 @@ int main() {
     int cant_peliculas;
     archivo >> cant_peliculas;
     archivo.ignore();
-    for (int i = 0; i < num_peliculas; i++) {
+    for (int i = 0; i < cant_peliculas; i++) {
         getline(archivo, linea);
         int pos1 = linea.find(';'); //busca el caracter ';' y regresa su posicion
         int pos2 = linea.find(';', pos1 + 1); //busca el caracter ';' justo despues del anterior
@@ -32,8 +24,37 @@ int main() {
         pelicula->director = linea.substr(pos1 + 1, pos2 - pos1 - 1); //obtiene el nombre del director
         pelicula->rating = stof(linea.substr(pos2 + 1)); //obtiene el ranking
         arbol.insertar_pelicula(pelicula); //inserta la pelicula en el arbol
-
     }
     archivo.close();
+    arbol.copiar_arbol();
+    string comando;
+    while (true){
+        cin >> comando;
+        if (comando.substr(0,2) == "sd") {
+            string nombre_director = comando.substr(3);
+            Director* director = arbol.buscar_director(nombre_director);
+            if (director) {
+                director->mostrar_peliculas();
+            }
+        }
+        else if (comando.substr(0,2) == "sm") {
+            string nombre_pelicula == comando.substr(3);
+            Pelicula* pelicula = arbol.buscar_pelicula(nombre_pelicula);
+            if (pelicula) {
+                cout << pelicula->nombre << " / " << pelicula->director << " / " << pelicula->rating << endl;
+            }
+        }
+        else if (comando.substr(0,2) == "br") {
+            int n = comando.substr(3);
+            arbol.mejores_directores(n);
+        }
+        else if (comando.substr(0,2) == "wr") {
+            int n = comando.substr(3);
+            arbol.peores_directores(n);
+        }
+        else if (comando.substr(0,1) == "e") {
+            break;
+        }
+    }
     return 0;
 }
