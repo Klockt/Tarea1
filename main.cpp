6b3418223e8dde1654d8457e4b5bcb15c5b94436
Amaro <aalarconl@usm.cl>
2024-11-21 17:24:42 -0300

    lectura de archivo

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include "class.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
 
 int main() {
-
+    ifstream archivo("Menu.txt");
+    if (!archivo.open()) {
+        cout << "Error al abrir el archivo" << endl;
+        return 1;
+    }
+    string linea;
+    while (getline(archivo, linea)) {
+        cout << linea << endl;
+    }
+    archivo.close();
+    return 0;
 }
