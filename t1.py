f57595bcfe5eb297dbf4113737908b7bd173845f
Mxim0 <Maximocastillo9@gmail.com>
2025-03-31 20:56:53 -0300

    final final

--- t1.py
+++ t1.py
@@ -7,12 +7,15 @@ import time
 binario = r'^[01]+$'
 octal = r'^[0-7]+$'
 hexa = r'^[0-9A-F]+$'
-template = "Ingresa una acción!\nw:moverse hacia arriba\ns:moverse hacia abajo\na:moverse a la izquierda\nd:moverse a la derecha\n-1:salir\n"
+template = "\nIngresa una acción!\nw:moverse hacia arriba\ns:moverse hacia abajo\na:moverse a la izquierda\nd:moverse a la derecha\n-1:salir\n"
 template2 = "Escribe la cantidad de pasos que quieres moverte hacia {} en formato {}: "
-template3 = "Esta transmisión contiene un código {} crítico: {}."
+template3 = "Esta transmisión está encriptada con un código {} crítico: {}."
+win_msj = "\033[33m\n¡Misión cumplida, Snake!\nHas descifrado el código con precisión quirúrgica.\nGracias a ti, el equipo podrá avanzar con la operación.\nEl Coronel está impresionado… ¡y yo también!\nPrepárate para la siguiente misión.\nEsto solo es el principio.\033[0m"
+lose_msj = "\033[31m\nSnake... fallaste en descifrar el código.\nLa operación fue comprometida, y el enemigo ahora tiene ventaja.\nNo todo está perdido, pero debes mantener la concentración.\nEl mundo depende de ti.\n¡Prepárate para intentarlo de nuevo!\n\033[0m"
 move = {"w":"arriba", "s":"abajo", "a":"la izquierda", "d":"la derecha", "-1":"salir"}
 hex_to_decimal = {"A": 10, "B": 11, "C": 12, "D": 13, "E": 14, "F": 15}
-snake_status = "alive" # alive, dead, hacking
+decimal_to_hex = {10: "A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F"}
+snake_status = "alive" # alive, dead, hacking, win
 snake_position = {"X": 5, "Y": 0}
 #------------------#
 
@@ -21,11 +24,10 @@ def print_board():
     os.system("clear")  
     for list in mx_b:
         print("".join(list))
-    print("\n")
     if snake_status == "dead":
         print("\033[31mSnaaaake! Snaaaaaake!\033[0m\nGame Over")
     elif snake_status == "hacking":
-        print("INICIANDO PROTOCOLO...\nAccediendo al sistema enemigo.\nCompilando datos sensibles...\n\033[32m'Snake, esto no tiene vuelta atrás... La misión depende de ti.'\033[0m\n[HACKING EN PROCESO]")
+        print("\nINICIANDO PROTOCOLO...\nAccediendo a la transmisión enemiga...\n\033[32m'Snake, esto no tiene vuelta atrás... La misión depende de ti.'\033[0m\n[HACKING EN PROCESO]\n")
 
 
 def matrix( large, guards ):
@@ -62,6 +64,25 @@ def generate_bin(decimal):
         bin_str += str(decimal % 2)
         decimal //= 2
     return bin_str[::-1]
+
+def generate_oct(decimal):
+    oct_str = ""
+    while decimal != 0:
+        oct_str += str(decimal % 8)
+        decimal //= 8
+    return oct_str[::-1]
+
+def generate_hex(decimal):
+    hex_str = ""
+    while decimal != 0:
+        rest =decimal % 16
+        if rest in decimal_to_hex:
+            hex_str += decimal_to_hex[rest]
+        else:
+            hex_str += str(rest)
+        decimal //= 16
+    return hex_str[::-1]
+
 
 def binary(bin_str):  # bin_str = "1101"
     '''
@@ -159,7 +180,7 @@ def snake_mov(direction, steps, large):  # X : Filas , Y : Columnas
 def main():
     large = int (input( "Ingresar largo de los pasillos: "))
     guards = int (input( "Ingresar cantidad de guardias: "))
-    global mx_b
+    global mx_b, snake_status
     mx_b = matrix( large, guards ) 
     for list in mx_b:
         list = "".join(list)
@@ -171,19 +192,19 @@ def main():
 
         if large <= 20:
             steps = input(template2.format((move[direction]), "Binario"))
-            while not re.match(binario, steps): # Para que no se cuele otra wea que no sea binario 8======D
+            while not re.match(binario, steps):
                 steps = input(template2.format((move[direction]), "Binario"))
             snake_mov(direction, binary(steps), large)
 
         elif large > 20 and large <= 100:
             steps = input(template2.format((move[direction]), "Octal"))
-            while not re.match(binario, steps):
+            while not re.match(octal, steps):
                 steps = input(template2.format((move[direction]), "Octal"))
             snake_mov(direction, oct(steps), large)
             
         elif large > 100:
             steps = input(template2.format((move[direction]), "Hexadecimal"))
-            while not re.match(binario, steps):
+            while not re.match(hexa, steps):
                 steps = input(template2.format((move[direction]), "Hexadecimal"))
             snake_mov(direction, hex(steps), large)
         
@@ -202,16 +223,40 @@ def main():
             print(template3.format("Binario", code))
             msj = input("Desencripta el mensaje y entrega el número decimal equivalente: ")
             if not re.match(r'^[0-9]+$', msj):
-                snake_status = "dead"
+                snake_status = "lose"
             elif code == generate_bin(int(msj)):
                 snake_status = "win"
             else:
-                snake_status = "dead"
+                snake_status = "lose"
+
         elif large > 20 and large <= 100:
             range = random.randint(0, 100)
+            code = generate_oct(range)
+            print(template3.format("Octal", code))
+            msj = input("Desencripta el mensaje y entrega el número decimal equivalente: ")
+            if not re.match(r'^[0-9]+$', msj):
+                snake_status = "lose"
+            elif code == generate_oct(int(msj)):
+                snake_status = "win"
+            else:
+                snake_status = "lose"
+
         elif large > 100:
             range = random.randint(0, 500)
-        
+            code = generate_hex(range)
+            print(template3.format("Hexadecimal", code))
+            msj = input("Desencripta el mensaje y entrega el número decimal equivalente: ")
+            if not re.match(r'^[0-9]+$', msj):
+                snake_status = "lose"
+            elif code == generate_hex(int(msj)):
+                snake_status = "win"
+            else:
+                snake_status = "lose"
+
+    if snake_status == "lose":
+        print(lose_msj)
+    elif snake_status == "win":
+        print(win_msj)
     return
 
 if __name__ == "__main__":
